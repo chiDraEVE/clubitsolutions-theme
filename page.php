ef81acecd0dbc59cf344b6db573e8411a2bba797
chidraeve <lukas.mainka@clubitsolutions.de>
2022-05-19 16:55:33 +0200

    corrected some strange behaviour in HTML structure. All pages loaded code from fictional-university. Now it's fixed and default pages got the template-part from the parrent theme. University pages get the template-part from content-page-university.php from child theme

--- page.php
+++ page.php
@@ -16,12 +16,15 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+	
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
+			global $isFictionalUniversity;
+			if ($isFictionalUniversity)
+				get_template_part( 'template-parts/content', 'page-university' );
+			else
+				get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
